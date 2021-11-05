f6bad58a8d7ea0812d1cf01318e81d83c6e0720f
Nick Tillinghast <nick.tillinghast@thekrazycouponlady.com>
2021-11-05 08:07:14 -0600

    add css and folders

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,23 @@
+import { Fragment } from 'react'
+import mealsImage from '../../assets/meals.jpeg';
+import classes from './Header.modules.css';
+
+
+const Header = props => {
+
+return (
+ <Fragment>
+    <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <button>Cart</button>
+
+    </header>
+    <div className={classes['main-image']}>
+        <img src={mealsImage} alt='table of food'/>
+    </div>
+
+</Fragment>
+)
+}
+
+export default Header
