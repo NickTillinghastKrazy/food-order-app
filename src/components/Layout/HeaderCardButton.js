89778949d0443301625d56cc03596fea18e7f3ce
Nick Tillinghast <nick.tillinghast@thekrazycouponlady.com>
2021-11-08 14:36:49 -0700

    added context and cart logic

--- src/components/Layout/HeaderCardButton.js
+++ src/components/Layout/HeaderCardButton.js
@@ -1,18 +1,35 @@
-import {useContext} from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CartIcon from '../Cart/CartIcon';
 import CartContext from '../../store/cart-context'
 import classes from './HeaderCardButton.module.css'
 
 const HeaderCardButton = props => {
-const cartCtx = useContext(CartContext);
-const numberOfCartItems = cartCtx.items.reduce((currentNumber,item) => {
+    const [ btnIsHighlighted, setbtnIsHighlighted ] = useState(false)
+    const cartCtx = useContext(CartContext);
+    
+    const { items } = cartCtx;
+    
+    const numberOfCartItems = cartCtx.items.reduce((currentNumber,item) => {
     return currentNumber + item.amount
 }, 0);
+const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`
 
+useEffect(() => {
+    if(items.length === 0) {
+        return;
+    }
+    setbtnIsHighlighted(true);
+   const timer = setTimeout(() => {
+        setbtnIsHighlighted(false)
+    }, 300);
+    return ( ) => {
+        clearTimeout(timer)
+    }
 
+}, [items]);
 
 return (
-<button className={classes.button} onClick={props.onClick}>
+<button className={btnClasses} onClick={props.onClick}>
     <span className={classes.icon}>
         <CartIcon />
     </span>
--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,10 +1,20 @@
+import { useContext } from 'react'
+import CartContext from '../../../store/cart-context'
 import classes from './MealItem.module.css'
 import MealItemForm from './MealItemForm'
 
 
 const MealItem = (props) => {
+   const cartCtx = useContext(CartContext);
 const price =`$${props.price.toFixed(2)}`
-const addToCartHandler = amount => {};
+const addToCartHandler = amount => {
+cartCtx.addItem({
+    id: props.id,
+    name: props.name,
+    amount: amount,
+    price: props.price
+})
+};
 
 return (
 <li className={classes.meal}>
